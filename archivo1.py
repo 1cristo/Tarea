9e56331176c7a041e3a17a0a44105b4f38509c95
Cristo <2020229@utsh.edu.mx>
2023-07-17 11:02:40 -0600

    Prueba 2

---
+++ archivo1.py
@@ -0,0 +1,4 @@
+nombre = input("Ingrese su nombre: ")  # Solicitar al usuario que ingrese su nombre
+saludo = f"Hola, {nombre} ¡Bienvenido!"  # Crear el mensaje de saludo dinámico
+
+print(saludo)  # Imprimir el saludo
